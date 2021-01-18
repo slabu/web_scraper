5d8310dfad01d6d2badfd8bf1a5e76ce074e1a22
slabu <serj.labunets@gmail.com>
2021-01-18 03:39:19 +0200

    Finished app

---
+++ templates/new_js.js
@@ -0,0 +1,62 @@
+fetch('results_javascript')
+    .then(function (response) {
+        return response.json();
+    }).then(function (text) {
+        console.log('GET response: ');
+        console.log(text);
+    });
+
+var elem = document.querySelector('.elem');
+
+var posts = {{ scraped_data| tojson }};
+
+resultsTable(elem, 4, 4);
+
+function resultsTable(parent, cols, rows) {
+    var table = document.createElement('table');
+
+    var header_tr = document.createElement('tr');
+
+    var post_id_th = document.createElement('th');
+    var post_id_th_text = document.createTextNode('post_id');
+    post_id_th.appendChild(post_id_th_text);
+    header_tr.appendChild(post_id_th);
+
+
+
+    for (var title in posts['post_1']) {
+
+        var post_title = document.createElement('th');
+        var post_title_text = document.createTextNode(title);
+        post_title.appendChild(post_title_text);
+        header_tr.append(post_title);
+
+        console.log(title)
+
+    }
+    table.appendChild(header_tr);
+
+
+
+    for (var key in posts) {
+        var tr = document.createElement('tr');
+        var post_id_td = document.createElement('td');
+        var post_id_text = document.createTextNode(key);
+        post_id_td.appendChild(post_id_text);
+        tr.appendChild(post_id_td);
+
+        for (var post in posts[key]) {
+
+            var td = document.createElement('td');
+            var cell_text = document.createTextNode(posts[key][post]);
+            td.appendChild(cell_text);
+            tr.appendChild(td);
+
+
+
+        }
+        table.appendChild(tr);
+    }
+    parent.appendChild(table);
+
+}
---
+++ web_scraping_script.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+#page_request = requests.get('https://www.ampereanalysis.com', verify=False)
+
+class WebScraper():
+    def __init__ (self, scraping_url):
+        self.scraping_url = scraping_url
+
+        self.scraping_method_results = self.scraping(self.scraping_url)
+        
+        
+    def scraping(self, scraping_url):
+        page_request = requests.get(scraping_url, verify=False)
+        soup = BeautifulSoup(page_request.text, "html.parser")
+
+        scraped_data = {}
+
+        post_number = 1
+        
+        for item in soup.find_all('div', {"class": "col-sm-12 col-md-3 pb-2"}):
+            date_and_author = item.span.text.split('\xa0\xa0')
+
+            scraped_data[f'post_{post_number}'] = {"Date": str(date_and_author[0]), "Author": ' '.join(date_and_author[1:]), "Title": item.h6.text}
+            post_number += 1
+        return scraped_data
